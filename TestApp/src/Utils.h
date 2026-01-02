8e19567f36dfd4cc733c80e15a9dbb1ec257e016
Pliexe <42877326+Pliexe@users.noreply.github.com>
2026-01-02 01:24:49 +0100

    Update tests

--- TestApp/src/Utils.h
+++ TestApp/src/Utils.h
@@ -1,21 +1,73 @@
+#include "Helios/Math/VectorImpl.h"
 #include <iostream>
 
+#define RED     "\033[31m"
+#define GREEN   "\033[32m"
+#define RESET   "\033[0m"   
+
+#define ORANGE  "\033[38;5;214m"
+#define YELLOW  "\033[38;5;226m"
+#define WHITE   "\033[38;5;15m"
+#define BLUE    "\033[38;5;15m"
+#define CYAN    "\033[36m"
+
+#define BG_RED  "\033[48;5;196m"
+#define BG_GREEN "\033[48;5;46m"
+#define BG_CYAN "\033[48;5;15m"
+
+#define EPSILON 1e-6
+
+#define TITLE(x) \
+    std::cout << "\n\t\033[1;33m" << x << "\033[0m\n" << std::endl;
+
+#define CRITICAL_TEST_APPROX(a, b) \
+    if (!((a - b).sqr_magnitude() < EPSILON * EPSILON)) { \
+        auto xa = (a); \
+        auto xb = (b); \
+        std::cerr << ORANGE "[CRITICAL] " RED "[FAIL] " RESET "[" BG_RED << __LINE__ << RESET << "] " << #a << RED " ≈ " RESET << #b << RESET \
+        << " (" RED << std::to_string(xa) << RESET " ≈ " GREEN << std::to_string(xb) << RESET ")" << RESET << std::endl; \
+        std::cout << "Critical test failed! Now exiting..." << std::endl; \
+        std::exit(1); \
+    } else { \
+        std::cout << ORANGE "[CRITICAL] " GREEN "[PASS] " RESET << #a << GREEN " ≈ " RESET << #b << RESET << std::endl; \
+    }
+
+#define CRITICAL_TEST_EQUALS(a, b) \
+    if (!(a == b)) { \
+        auto xa = (a); \
+        auto xb = (b); \
+        std::cerr << ORANGE "[CRITICAL] " RED "[FAIL] " RESET "[" BG_RED << __LINE__ << RESET << "] " << #a << RED " == " RESET << #b << RESET \
+        << " (" RED << std::to_string(xa) << RESET " == " GREEN << std::to_string(xb) << RESET ")" << RESET << std::endl; \
+        std::cout << "Critical test failed! Now exiting..." << std::endl; \
+        std::exit(1); \
+    } else { \
+        std::cout << ORANGE "[CRITICAL] " GREEN "[PASS] " RESET << #a << GREEN " == " RESET << #b << RESET << std::endl; \
+    }
+
+#define CRITICAL_TEST_NOT_EQUALS(a, b) \
+    if (!(a != b)) { \
+        auto xa = (a); \
+        auto xb = (b); \
+        std::cerr << ORANGE "[CRITICAL] " RED "[FAIL] " RESET "[" BG_RED << __LINE__ << RESET << "] " << #a << RED " != " RESET << #b << RESET \
+        << " (" RED << std::to_string(xa) << RESET " != " GREEN << std::to_string(xb) << RESET ")" << RESET << std::endl; \
+        std::cout << "Critical test failed! Now exiting..." << std::endl; \
+        std::exit(1); \
+    } else { \
+        std::cout << ORANGE "[CRITICAL] " GREEN "[PASS] " RESET << #a << GREEN " == " RESET << #b << RESET << std::endl; \
+    }
+
 #define CRITICAL_TEST(expr) \
-    do { \
-        if (!(expr)) { \
-            std::cerr << "[CRITICAL] [FAIL] " #expr << std::endl; \
-            std::cout << "Critical test failed! Now existing..." << std::endl; \
-            std::exit(1); \
-        } else { \
-            std::cout << "[CRITICAL] [PASS] " #expr << std::endl; \
-        } \
-    } while(0)
+    if (!(expr)) { \
+        std::cerr << ORANGE "[CRITICAL] " RED "[FAIL] " RESET "[" BG_RED << __LINE__ << RESET << "] " #expr << RESET << std::endl; \
+        std::cout << "Critical test failed! Now exiting..." << std::endl; \
+        std::exit(1); \
+    } else { \
+        std::cout << ORANGE "[CRITICAL] " GREEN "[PASS] " RESET << #expr << RESET << std::endl; \
+    }
 
 #define MINOR_TEST(expr) \
-    do { \
-        if (!(expr)) { \
-            std::cerr << "[MINOR] [FAIL] " #expr << std::endl; \
-        } else { \
-            std::cout << "[MINOR] [PASS] " #expr << std::endl; \
-        } \
-    } while(0)
+    if (!(expr)) { \
+        std::cerr << YELLOW "[MINOR] " RED "[FAIL] " RESET "[" BG_RED << __LINE__ << RESET << "] " #expr << RESET << std::endl; \
+    } else { \
+        std::cout << YELLOW "[MINOR] " GREEN "[PASS] " RESET << #expr << RESET << std::endl; \
+    }
--- TestApp/src/main.cpp
+++ TestApp/src/main.cpp
@@ -2,6 +2,7 @@
 
 #include <Helios/Core/UUID.h>
 #include <Helios.h>
+#include "Helios/Math/VectorImpl.h"
 #include "Utils.h"
 
 Helios::Application *Helios::CreateApplication(int argc, char **argv)
@@ -9,11 +10,49 @@ Helios::Application *Helios::CreateApplication(int argc, char **argv)
 	return nullptr;
 }
 
+namespace std
+{
+    template <typename T, std::size_t S, bool IsVector>
+    inline std::string to_string(const Helios::TVector<T, S, IsVector>& vector) {
+        std::ostringstream ss;
+        ss << vector.to_string();
+        return ss.str();
+    }
+
+    inline std::string to_string(const Helios::UUID& uuid) {
+        std::ostringstream ss;
+        ss << uuid.toString();
+        return ss.str();
+    }
+}
+
 int main(int, char **)
 {
-    std::cout << "Starting UnitTests:" << std::endl;
+    TITLE("Starting UnitTests");
 
-    // CRITICAL_TEST(Helios::UUID() != Helios::UUID());
+    TITLE("UUID Tests");
+
+    CRITICAL_TEST(Helios::UUID() != Helios::UUID());
+    
+    Helios::UUID uuid = Helios::UUID();
+    Helios::UUID uuid2 = Helios::UUID();
+    Helios::UUID uuid3 = Helios::UUID(uuid);
+
+    std::cout << "UUID: " << uuid.toString() << std::endl;
+    std::cout << "UUID2: " << uuid2.toString() << std::endl;
+    std::cout << "UUID3: " << uuid3.toString() << std::endl;
+
+    CRITICAL_TEST_EQUALS(uuid, uuid);
+    CRITICAL_TEST_NOT_EQUALS(uuid, uuid2);
+    CRITICAL_TEST_EQUALS(uuid, uuid3);
+
+    uint8_t data[16];
+    std::memcpy(data, (uint8_t*)uuid, 16);
+
+    Helios::UUID uuid4 = Helios::UUID::fromString(uuid.toString());
+    CRITICAL_TEST(uuid4 == uuid);
+
+    TITLE("Quaternion Tests");
 
     Helios::Quaternion q;
     q.x = 0;
@@ -21,19 +60,214 @@ int main(int, char **)
     q.z = 0;
     q.w = 1;
 
-    auto forward = q.forward();
-    auto right = q.right();
-    auto up = q.up();
+    CRITICAL_TEST(q.forward() == Helios::Vector3(0, 0, 1));
+    CRITICAL_TEST(q.right() == Helios::Vector3(1, 0,  0));
+    CRITICAL_TEST(q.up() == Helios::Vector3(0, 1,  0));
 
-    std::cout << "Forward: " << forward.to_string() << std::endl;
-    std::cout << "Right: " << right.to_string() << std::endl;
-    std::cout << "Up: " << up.to_string() << std::endl;
+    TITLE("Vector Tests");
 
-    assert(forward == Helios::Vector3(0, 0, 1));
-    assert(right == Helios::Vector3(1, 0,  0));
-    assert(up == Helios::Vector3(0, 1,  0));
+    TITLE("Vector2 Equality Tests");
 
-    std::cout << "Tests passed!" << std::endl;
+    CRITICAL_TEST(Helios::Vector2(1, 0) == Helios::Vector2(1, 0));
+    CRITICAL_TEST(Helios::Vector2(0, 1) == Helios::Vector2(0, 1));
+    CRITICAL_TEST(Helios::Vector2(-1, 0) == Helios::Vector2(-1, 0));
+    CRITICAL_TEST(Helios::Vector2(0, -1) == Helios::Vector2(0, -1));
+
+    TITLE("Vector3 Equality Tests");
+    
+    CRITICAL_TEST(Helios::Vector3(1, 0, 0) == Helios::Vector3(1, 0, 0));
+    CRITICAL_TEST(Helios::Vector3(0, 1, 0) == Helios::Vector3(0, 1, 0));
+    CRITICAL_TEST(Helios::Vector3(0, 0, 1) == Helios::Vector3(0, 0, 1));
+    CRITICAL_TEST(Helios::Vector3(-1, 0, 0) == Helios::Vector3(-1, 0, 0));
+    CRITICAL_TEST(Helios::Vector3(0, -1, 0) == Helios::Vector3(0, -1, 0));
+    CRITICAL_TEST(Helios::Vector3(0, 0, -1) == Helios::Vector3(0, 0, -1));
+
+    TITLE("Vector4 Equality Tests");
+
+    CRITICAL_TEST(Helios::Vector4(1, 0, 0, 0) == Helios::Vector4(1, 0, 0, 0));
+    CRITICAL_TEST(Helios::Vector4(0, 1, 0, 0) == Helios::Vector4(0, 1, 0, 0));
+    CRITICAL_TEST(Helios::Vector4(0, 0, 1, 0) == Helios::Vector4(0, 0, 1, 0));
+    CRITICAL_TEST(Helios::Vector4(0, 0, 0, 1) == Helios::Vector4(0, 0, 0, 1));
+    CRITICAL_TEST(Helios::Vector4(-1, 0, 0, 0) == Helios::Vector4(-1, 0, 0, 0));
+    CRITICAL_TEST(Helios::Vector4(0, -1, 0, 0) == Helios::Vector4(0, -1, 0, 0));
+    CRITICAL_TEST(Helios::Vector4(0, 0, -1, 0) == Helios::Vector4(0, 0, -1, 0));
+    CRITICAL_TEST(Helios::Vector4(0, 0, 0, -1) == Helios::Vector4(0, 0, 0, -1));
+
+    TITLE("Vector3 Constants Tests");
+
+    CRITICAL_TEST_EQUALS(Helios::Vector3::Forward(), Helios::Vector3(0, 0, 1));
+    CRITICAL_TEST_EQUALS(Helios::Vector3::Back(), Helios::Vector3(0, 0, -1));
+    CRITICAL_TEST_EQUALS(Helios::Vector3::Right(), Helios::Vector3(1, 0, 0));
+    CRITICAL_TEST_EQUALS(Helios::Vector3::Up(), Helios::Vector3(0, 1, 0));
+    CRITICAL_TEST_EQUALS(Helios::Vector3::Left(), Helios::Vector3(-1, 0, 0));
+    CRITICAL_TEST_EQUALS(Helios::Vector3::Down(), Helios::Vector3(0, -1, 0));
+    CRITICAL_TEST_EQUALS(Helios::Vector3::One(), Helios::Vector3(1, 1, 1));
+    CRITICAL_TEST_EQUALS(Helios::Vector3::Zero(), Helios::Vector3(0, 0, 0));
+
+    TITLE("Vector2 Constants Tests");
+
+    CRITICAL_TEST_EQUALS(Helios::Vector2::Up(), Helios::Vector2(0, 1));
+    CRITICAL_TEST_EQUALS(Helios::Vector2::Down(), Helios::Vector2(0, -1));
+    CRITICAL_TEST_EQUALS(Helios::Vector2::Right(), Helios::Vector2(1, 0));
+    CRITICAL_TEST_EQUALS(Helios::Vector2::Left(), Helios::Vector2(-1, 0));
+    CRITICAL_TEST_EQUALS(Helios::Vector2::One(), Helios::Vector2(1, 1));
+    CRITICAL_TEST_EQUALS(Helios::Vector2::Zero(), Helios::Vector2(0, 0));
+
+    {
+        TITLE("Vector operations Tests");
+
+        auto v1 = Helios::Vector3(1, 0, 0);
+        auto v2 = Helios::Vector3(0, 1, 0);
+        auto v3 = Helios::Vector3(10, 5, 15);
+        auto v4 = 5.f;
+        auto v5 = Helios::Vector3(2, 0, 0);
+        
+        CRITICAL_TEST_EQUALS(v1 + v2, Helios::Vector3(1, 1, 0));
+        CRITICAL_TEST_EQUALS(v1 - v2, Helios::Vector3(1, -1, 0));
+        CRITICAL_TEST_EQUALS(v1 * v4, Helios::Vector3(5, 0, 0));
+        CRITICAL_TEST_EQUALS(v1 / v4, Helios::Vector3(0.2f, 0, 0));
+
+        CRITICAL_TEST_EQUALS(v3 + v3, Helios::Vector3(20, 10, 30));
+        CRITICAL_TEST_EQUALS(v3 - v3, Helios::Vector3(0, 0, 0));
+        CRITICAL_TEST_EQUALS(v3 * v4, Helios::Vector3(50, 25, 75));
+        CRITICAL_TEST_EQUALS(v3 / v4, Helios::Vector3(2, 1, 3));
+
+        CRITICAL_TEST_EQUALS(v1.magnitude(), 1);
+        CRITICAL_TEST_EQUALS(v2.magnitude(), 1);
+        CRITICAL_TEST_EQUALS(v3.magnitude(), std::sqrt(15.f * 15.f + 5.f * 5.f + 10.f * 10.f));
+
+        CRITICAL_TEST_EQUALS(v1.normalized(), v1 / v1.magnitude());
+        CRITICAL_TEST_EQUALS(v2.normalized(), v2 / v2.magnitude());
+        CRITICAL_TEST_EQUALS(v3.normalized(), v3 / v3.magnitude());
+
+        CRITICAL_TEST_EQUALS(Helios::Vector3::Distance(v1, v2), (v1 - v2).magnitude());
+        CRITICAL_TEST_EQUALS(Helios::Vector3::Distance(v1, v3), (v1 - v3).magnitude());
+        CRITICAL_TEST_EQUALS(Helios::Vector3::Distance(v2, v3), (v2 - v3).magnitude());
+
+        CRITICAL_TEST_EQUALS(Helios::Vector3::Dot(v1, v2), 0);
+        CRITICAL_TEST_EQUALS(Helios::Vector3::Dot(v1, v3), 10);
+        CRITICAL_TEST_EQUALS(Helios::Vector3::Dot(v2, v3), 5);
+        CRITICAL_TEST_EQUALS(Helios::Vector3::Dot(v3, v3), 15.f * 15.f + 5.f * 5.f + 10.f * 10.f);
+        
+        CRITICAL_TEST_EQUALS(Helios::Vector3::Scale(v1, v2), Helios::Vector3(0, 0, 0));
+        CRITICAL_TEST_EQUALS(Helios::Vector3::Scale(v1, v3), Helios::Vector3(10, 0, 0));
+        CRITICAL_TEST_EQUALS(Helios::Vector3::Scale(v2, v3), Helios::Vector3(0, 5, 0));
+        CRITICAL_TEST_EQUALS(Helios::Vector3::Scale(v3, v3), Helios::Vector3(v3.x * v3.x, v3.y * v3.y, v3.z * v3.z));
+
+        CRITICAL_TEST_EQUALS(Helios::Vector3::Divide(v1, v3), Helios::Vector3(v1.x / v3.x, v1.y / v3.y, v1.z / v3.z));
+        CRITICAL_TEST_EQUALS(Helios::Vector3::Divide(v2, v3), Helios::Vector3(v2.x / v3.x, v2.y / v3.y, v2.z / v3.z));
+        CRITICAL_TEST_EQUALS(Helios::Vector3::Divide(v3, v3), Helios::Vector3(v3.x / v3.x, v3.y / v3.y, v3.z / v3.z));
+
+        CRITICAL_TEST_EQUALS(Helios::Vector3::MoveTowards(v1, v5, 0.5f), Helios::Vector3(1 + 0.5f, 0, 0));
+        CRITICAL_TEST_EQUALS(Helios::Vector3::MoveTowards(v1, v5, 1.5f), Helios::Vector3(1 + 1.5f, 0, 0));
+        CRITICAL_TEST_EQUALS(Helios::Vector3::MoveTowards(v1, v5, 2.5f), Helios::Vector3(1 + 2.5f, 0, 0));
+
+        CRITICAL_TEST_EQUALS(Helios::Vector3::Lerp(v1, v2, 0.5f), Helios::Vector3(0.5f, 0.5f, 0));
+        CRITICAL_TEST_EQUALS(Helios::Vector3::Lerp(v1, v2, 1.5f), v2);
+        CRITICAL_TEST_EQUALS(Helios::Vector3::Lerp(v1, v2, 2.5f), v2);
+        CRITICAL_TEST_EQUALS(Helios::Vector3::Lerp(v1, v3, 0.5f), Helios::Vector3(5.5f, 2.5f, 7.5f));
+        CRITICAL_TEST_EQUALS(Helios::Vector3::Lerp(v1, v3, 1.5f), v3);
+        CRITICAL_TEST_EQUALS(Helios::Vector3::Lerp(v1, v3, 2.5f), v3);
+
+        CRITICAL_TEST_EQUALS(Helios::Vector3::LerpUnclamped(v1, v2, 0.5f), Helios::Vector3(0.5f, 0.5f, 0));
+        CRITICAL_TEST_EQUALS(Helios::Vector3::LerpUnclamped(v1, v2, 1.5f), v2 + (v2 - v1) * 0.5f);
+        CRITICAL_TEST_EQUALS(Helios::Vector3::LerpUnclamped(v1, v2, 2.5f), v2 + (v2 - v1) * 1.5f);
+        CRITICAL_TEST_EQUALS(Helios::Vector3::LerpUnclamped(v1, v2, 3.5f), v2 + (v2 - v1) * 2.5f);
+        CRITICAL_TEST_EQUALS(Helios::Vector3::LerpUnclamped(v1, v3, 0.5f), Helios::Vector3(5.5f, 2.5f, 7.5f));
+        CRITICAL_TEST_EQUALS(Helios::Vector3::LerpUnclamped(v1, v3, 1.5f), v3 + (v3 - v1) * 0.5f);
+        CRITICAL_TEST_EQUALS(Helios::Vector3::LerpUnclamped(v1, v3, 2.5f), v3 + (v3 - v1) * 1.5f);
+        CRITICAL_TEST_EQUALS(Helios::Vector3::LerpUnclamped(v1, v3, 3.5f), v3 + (v3 - v1) * 2.5f);
+
+        CRITICAL_TEST_EQUALS(Helios::Vector3::Reflect(v1, v2), v1 - v2 * 2.f * Helios::Vector3::Dot(v1, v2));
+        CRITICAL_TEST_EQUALS(Helios::Vector3::Reflect(v1, v3), v1 - v3 * 2.f * Helios::Vector3::Dot(v1, v3));
+        CRITICAL_TEST_EQUALS(Helios::Vector3::Reflect(v2, v3), v2 - v3 * 2.f * Helios::Vector3::Dot(v2, v3));
+        CRITICAL_TEST_EQUALS(Helios::Vector3::Reflect(v3, v3), v3 - v3 * 2.f * Helios::Vector3::Dot(v3, v3));
+
+        CRITICAL_TEST_EQUALS(Helios::Vector3::Max(v1, v2), Helios::Vector3(1, 1, 0));
+        CRITICAL_TEST_EQUALS(Helios::Vector3::Max(v1, v3), v3);
+        CRITICAL_TEST_EQUALS(Helios::Vector3::Max(v2, v3), v3);
+        CRITICAL_TEST_EQUALS(Helios::Vector3::Max(v3, v3), v3);
+
+        CRITICAL_TEST_EQUALS(Helios::Vector3::Min(v1, v2), Helios::Vector3(0, 0, 0));
+        CRITICAL_TEST_EQUALS(Helios::Vector3::Min(v1, v3), Helios::Vector3(1, 0, 0));
+        CRITICAL_TEST_EQUALS(Helios::Vector3::Min(v2, v3), Helios::Vector3(0, 1, 0));
+        CRITICAL_TEST_EQUALS(Helios::Vector3::Min(v3, v3), v3);
+
+        CRITICAL_TEST_EQUALS(Helios::Vector3::Clamp(v1, v2, v3), Helios::Vector3(1, 1, 0));
+        CRITICAL_TEST_EQUALS(Helios::Vector3::Clamp(v2, v1, v3), Helios::Vector3(1, 1, 0));
+
+        CRITICAL_TEST_EQUALS(Helios::Vector3::ClampMagnitude(v1, 1), v1);
+        CRITICAL_TEST_EQUALS(Helios::Vector3::ClampMagnitude(v2, 1), v2);
+        CRITICAL_TEST_EQUALS(Helios::Vector3::ClampMagnitude(v3, 1), Helios::Vector3(1, 1, 1));
+        CRITICAL_TEST_EQUALS(Helios::Vector3::ClampMagnitude(v3, 2), Helios::Vector3(2, 2, 2));
+        CRITICAL_TEST_EQUALS(Helios::Vector3::ClampMagnitude(v3, 15), v3);
+
+        CRITICAL_TEST_EQUALS(Helios::Vector3::Project(v1, v2), Helios::Vector3(0, 0, 0));
+        CRITICAL_TEST_APPROX(Helios::Vector3::Project(v1, v3), Helios::Vector3(0.285714f, 0.142857f, 0.428571f));
+        CRITICAL_TEST_APPROX(Helios::Vector3::Project(v2, v3), Helios::Vector3(0.142857f, 0.071429f, 0.214286f));
+        CRITICAL_TEST_EQUALS(Helios::Vector3::Project(v3, v3), v3);
+
+        CRITICAL_TEST_APPROX(Helios::Vector3::ProjectOnPlane(v1, v2, v3), Helios::Vector3(10, 0, 15));
+        CRITICAL_TEST_APPROX(Helios::Vector3::ProjectOnPlane(v1, v3, v3), Helios::Vector3(0.2857142857, 0.1428571429, 0.4285714286));
+        CRITICAL_TEST_APPROX(Helios::Vector3::ProjectOnPlane(v2, v3, v3), Helios::Vector3(0.1428571429, 0.0714285714, 0.2142857143));
+        CRITICAL_TEST_APPROX(Helios::Vector3::ProjectOnPlane(v3, v3, v3), v3);
+
+        auto rayOrigin = Helios::Vector3(1,2,10);
+        auto rayDirection = Helios::Vector3(0,0,-1);
+        auto planeNormal = Helios::Vector3(0,0,1);
+        auto planePoint = Helios::Vector3(0,0,0);
+
+        CRITICAL_TEST_APPROX(
+            Helios::Vector3::ProjectRayOnPlane(rayOrigin, rayDirection, planeNormal, planePoint),
+            Helios::Vector3(1,2,20)
+        );
+
+        rayOrigin = Helios::Vector3(5,10,3);
+        rayDirection = Helios::Vector3(0,-1,0);
+        planeNormal = Helios::Vector3(0,1,0);
+        planePoint = Helios::Vector3(0,0,0);
+
+        CRITICAL_TEST_APPROX(
+            Helios::Vector3::ProjectRayOnPlane(rayOrigin, rayDirection, planeNormal, planePoint),
+            Helios::Vector3(5,20,3)
+        );
+
+        rayOrigin = Helios::Vector3(0,0,0);
+        rayDirection = Helios::Vector3(1,1,1);
+        planeNormal = Helios::Vector3(0,0,1);
+        planePoint = Helios::Vector3(0,0,5);
+
+        CRITICAL_TEST_APPROX(
+            Helios::Vector3::ProjectRayOnPlane(rayOrigin, rayDirection, planeNormal, planePoint),
+            Helios::Vector3(-5,-5,-5)
+        );
+
+        rayOrigin = Helios::Vector3(1,2,5);
+        rayDirection = Helios::Vector3(1,0,0);
+        planeNormal = Helios::Vector3(0,0,1);
+        planePoint = Helios::Vector3(0,0,5);
+
+        Helios::Vector3 result;
+
+        CRITICAL_TEST_EQUALS(
+            (result = Helios::Vector3::ProjectRayOnPlane(rayOrigin, rayDirection, planeNormal, planePoint), true),
+            Helios::Vector3::IsNaN(result)
+        );
+
+
+        TITLE("Vector3 operations Tests");
+
+        CRITICAL_TEST_EQUALS(Helios::Vector3::Angle(v1, v2), std::atan2(v2.y, v2.x) - std::atan2(v1.y, v1.x));
+        CRITICAL_TEST_EQUALS(Helios::Vector3::Angle(v1, v3), std::atan2(Helios::Vector3::Cross(v1, v3).magnitude(), Helios::Vector3::Dot(v1, v3)));
+        CRITICAL_TEST_EQUALS(Helios::Vector3::Angle(v2, v3), std::atan2(Helios::Vector3::Cross(v2, v3).magnitude(), Helios::Vector3::Dot(v2, v3)));
+        CRITICAL_TEST_EQUALS(Helios::Vector3::Angle(v3, v3), 0);
+
+        CRITICAL_TEST_EQUALS(Helios::Vector3::Cross(v1, v2), Helios::Vector3(0, 0, 1));
+        CRITICAL_TEST_EQUALS(Helios::Vector3::Cross(v1, v3), Helios::Vector3(0, -15, 5));
+        CRITICAL_TEST_EQUALS(Helios::Vector3::Cross(v2, v3), Helios::Vector3(15, 0, -10));
+        CRITICAL_TEST_EQUALS(Helios::Vector3::Cross(v3, v3), Helios::Vector3(0, 0, 0));
+        
+    }
 
     return 0;
 }
